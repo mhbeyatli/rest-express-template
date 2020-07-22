c43bf8565ce6ca335d80b3b86eba9470c0c6b301
habib.beyatli <habibbeyatli@gmail.com>
2020-07-22 20:11:03 +0300

    Check-auth middleware is implemented

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose')
+const checkAuth = require('../middleware/check-auth')
 
 const Order = require('../models/orders')
 const Product = require('../models/product');
 const product = require('../models/product');
 
-router.get('/', (req, res, next) => {
+router.get('/', checkAuth, (req, res, next) => {
   Order.find()
     .select('product quantity _id')
     .populate('product', 'name')
@@ -34,7 +35,7 @@ router.get('/', (req, res, next) => {
     })
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAuth, (req, res, next) => {
   Product.findById(req.body.productId)
     .then(product => {
       if(!product) {
@@ -73,12 +74,12 @@ router.post('/', (req, res, next) => {
 
 });
 
-router.get('/:orderId', (req, res, next) => {
-  Order.findById(req.body.orderId)
+router.get('/:orderId', checkAuth, (req, res, next) => {
+  Order.findById(req.params.orderId)
     .populate('product')
     .exec()
     .then(order => {
-      if(!order) {
+      if (!order) {
         return res.status(404).json({
           message: 'Order not found'
         })
@@ -98,7 +99,7 @@ router.get('/:orderId', (req, res, next) => {
     })
 });
 
-router.delete('/:orderId', (req, res, next) => {
+router.delete('/:orderId', checkAuth, (req, res, next) => {
   Order.remove({ _id: req.params.orderId })
     .exec()
     .then(result => {
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose')
+const checkAuth = require('../middleware/check-auth')
 
 const Product = require('../models/product')
 
@@ -18,7 +19,7 @@ router.get('/', (req, res, next) => {
     })
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAuth, (req, res, next) => {
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
@@ -59,7 +60,7 @@ router.get('/:productId', (req, res, next) => {
     })
 });
 
-router.patch('/:productId', (req, res, next) => {
+router.patch('/:productId', checkAuth, (req, res, next) => {
   const id = req.params.productId
   const updateOps = {}
   for (const key of Object.keys(req.body)) {
@@ -79,7 +80,7 @@ router.patch('/:productId', (req, res, next) => {
 
 });
 
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:productId', checkAuth, (req, res, next) => {
   const id = req.params.productId
   Product
     .remove({_id: id})
