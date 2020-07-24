586052ed0387f8593edd90967327d10bc4226a8a
habib.beyatli <habibbeyatli@gmail.com>
2020-07-24 23:27:11 +0300

    Controllers are added

---
+++ api/controllers/products.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose')
+
+const Product = require('../models/product')
+
+exports.get_all_products = (req, res, next) => {
+  Product
+    .find()
+    .exec()
+    .then(docs => {
+      console.log(docs)
+      res.status(200).json(docs)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({error: err})
+    })
+}
+
+exports.create_product = (req, res, next) => {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
+  })
+  
+  product
+    .save()
+    .then(result => {
+      console.log("result",result)
+      res.status(201).json({
+        message: 'Product is created',
+        createdProduct: product
+      });
+    })
+    .catch(err => {
+      console.log("err",err)
+      res.status(500).json({error: err})
+    })
+}
+
+exports.get_one_product = (req, res, next) => {
+  const id = req.params.productId;
+  Product
+    .findById(id)
+    .exec()
+    .then(doc => { 
+      if (doc) {
+        res.status(200).json({ doc: doc })
+      } else {
+        res.status(404).json({message: 'Given Id is not found'})
+      }
+      console.log("doc", doc)
+    })
+    .catch(err => {
+      res.status(500).json({error: err})
+    })
+}
+
+exports.update_product = (req, res, next) => {
+  const id = req.params.productId
+  const updateOps = {}
+  for (const key of Object.keys(req.body)) {
+    updateOps[key] = req.body[key]
+  }
+  Product
+    .update({_id: id}, {$set: updateOps})
+    .exec()
+    .then(result => {
+      console.log(result)
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({error: err})
+    })
+}
+
+exports.delete_product = (req, res, next) => {
+  const id = req.params.productId
+  Product
+    .remove({_id: id})
+    .exec()
+    .then(result => console.log(result))
+    .catch(err => {
+      res.status(500).json({error: err})
+    })
+}
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,95 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose')
+
 const checkAuth = require('../middleware/check-auth')
+const ProductsController = require('../controllers/products')
 
-const Product = require('../models/product')
+router.get('/', ProductsController.get_all_products);
 
-router.get('/', (req, res, next) => {
-  Product
-    .find()
-    .exec()
-    .then(docs => {
-      console.log(docs)
-      res.status(200).json(docs)
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({error: err})
-    })
-});
+router.post('/', checkAuth, ProductsController.create_product);
 
-router.post('/', checkAuth, (req, res, next) => {
-  const product = new Product({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    price: req.body.price
-  })
-  
-  product
-    .save()
-    .then(result => {
-      console.log("result",result)
-      res.status(201).json({
-        message: 'Handling POST request to /products',
-        createdProduct: product
-      });
-    })
-    .catch(err => {
-      console.log("err",err)
-      res.status(500).json({error: err})
-    })
-});
+router.get('/:productId', ProductsController.get_one_product);
 
-router.get('/:productId', (req, res, next) => {
-  const id = req.params.productId;
-  Product
-    .findById(id)
-    .exec()
-    .then(doc => { 
-      if (doc) {
-        res.status(200).json({ doc: doc })
-      } else {
-        res.status(404).json({message: 'Given Id is not found'})
-      }
-      console.log("doc", doc)
-    })
-    .catch(err => {
-      res.status(500).json({error: err})
-      console.log(err)
-    })
-});
+router.patch('/:productId', checkAuth, ProductsController.update_product);
 
-router.patch('/:productId', checkAuth, (req, res, next) => {
-  const id = req.params.productId
-  const updateOps = {}
-  for (const key of Object.keys(req.body)) {
-    updateOps[key] = req.body[key]
-  }
-  Product
-    .update({_id: id}, {$set: updateOps})
-    .exec()
-    .then(result => {
-      console.log(result)
-      res.status(200).json(result)
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({error: err})
-    })
-
-});
-
-router.delete('/:productId', checkAuth, (req, res, next) => {
-  const id = req.params.productId
-  Product
-    .remove({_id: id})
-    .exec()
-    .then(result => console.log(result))
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({error: err})
-    })
-});
+router.delete('/:productId', checkAuth, ProductsController.delete_product);
 
 module.exports = router;
